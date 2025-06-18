d3606b25d4f83cbe5dfab17408d53806879439cb
ssomeshetty <bremblue189@gmail.com>
2025-06-19 00:00:27 +0530

    admin added

--- django-backend/spoof_detector/detector_api/admin.py
+++ django-backend/spoof_detector/detector_api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import DetectionResult
 
-# Register your models here.
+@admin.register(DetectionResult)
+class DetectionResultAdmin(admin.ModelAdmin):
+    list_display = ('user_id', 'confidence', 'is_real', 'status', 'timestamp')
+    list_filter = ('is_real', 'status', 'timestamp')
+    search_fields = ('user_id', 'status')
+    ordering = ('-timestamp',)
